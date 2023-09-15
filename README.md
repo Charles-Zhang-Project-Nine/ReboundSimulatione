58c892ad23b07b1af40fe2db688d97886b26e0fc
Charles Zhang <szinubuntu@gmail.com>
2023-09-15 16:16:09 -0400

    [Doma] Experiment setup and tooling (visualization)
    - Streamline experiment code setup
    - Establish std output formats
    - Create blender python visualization script
    - Update make script

--- README.md
+++ README.md
@@ -2,11 +2,14 @@
 
 Project Rebound utilizes Hanno Rein 's N-body [simulation library](https://github.com/hannorein/rebound) for the purpose of simulation setup for systems within Project Nine ("Simulatione"). 
 The immediate goal is to find stable configurations and predicted future trajections for the Doma and X13 systems. Consult P9 for more detailed definition on this project.
+This repo is pure simulation and outputs data points. A seperate parser/analyzer/visualizer will be written (likely in Pure/C#/Python) for analyzing those data.
 
 Notice due to the nature of a public fork, this repo is public. In that case, we should avoid referencing Project Nine contents and try to keep this repo a pure-simulation environment.
 
 This code follows original repository setup and utilizes folders inside "examples" For experiment setups. To compile and run any experiment, go to corresponding experiment folder, perform a `make` and run. The C codes will only work on Linux.  
 From a software-development perspective, the best way to utilize this is to: 1) Data-driven everything first, 2) Develop and package the software as a proper CLI tool. However, for the purpose of this project, we will just edit in-place and use as-is for immedaite needs. The "tooling" aspect can be done in a seperate fork. See [OriginalREADME](./OriginalREADME.md) for original README file.
+
+Units conventions: Notice REBOUND is designed to work with $G=1$. It's good and makes simulation independent of actual magnitude of things and we should get used to this convention, where both mass and distance are relative.
 
 Below are the Project Nine specific setups:
 
--- examples/doma/problem.c
+++ examples/doma/problem.c
@@ -7,31 +7,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void simulate(struct reb_simulation* simulation, double timestep)
+{
+    // reb_integrate(simulation, timestep);
+    reb_steps(simulation, timestep);
+}
+
+void print_coordinates(struct reb_simulation* simulation, int frame, const char* names[])
+{
+    for (int i = 0; i < simulation->N; i++){
+        struct reb_particle p = simulation->particles[i];
+        // Format: Particle Frame X Y Z
+        printf("%s %i %f %f %f\n", names[i], frame, p.x, p.y, p.z);
+    }
+}
+void print_orbits(struct reb_simulation* simulation, const char* names[])
+{
+    struct reb_particle primary = simulation->particles[0];
+    struct reb_particle* particles = simulation->particles;
+
+    for (int i = 1; i < simulation->N; i++){
+        struct reb_particle p = particles[i];
+        struct reb_orbit o = reb_tools_particle_to_orbit(simulation->G, p, primary);
+        // Format: Particle Semi-major axis, elevation, true anomaly
+        printf("%s %f %f %f\n", names[i], o.a, o.e, o.f);
+    }
+}
+
 int main(int argc, char* argv[]) {
     // Initialize
     struct reb_simulation* r = reb_create_simulation();
 
+    // Parameters
+    int iterations = 50;
+    double timestep = 50.;
+
     // Setup
-    reb_add_fmt(r, "m", 1.);                // Central object
-    reb_add_fmt(r, "m a e", 1e-3, 1., 0.1); // Jupiter mass planet
-    reb_add_fmt(r, "a e", 1.4, 0.1);        // Massless test particle 
+    const char* names[] = { "Doma", "Ids", "Fukxim" };
+    reb_add_fmt(r, "m", 1.0);                    // Doma
+    reb_add_fmt(r, "m a e", 1e-3, 1.0, 0.1);     // Ids
+    reb_add_fmt(r, "m a e", 2e-3, -1.4, 1.1);    // Fukxim 
 
+    // Meta-data
+    // Format: Key: Value
+    printf("# Metadata\n");
+    printf("@ Objects: ");
+    unsigned long objects = sizeof(names) / sizeof(const char*);
+    for(unsigned long i = 0; i < objects; i++)
+        printf("%s ", names[i]);
+    printf("\n@ Frames: %i\n", iterations);
+
+    // Initial position
+    printf("# Initial Positions: <Name> <Frame> <X> <Y> <Z>\n");
+    print_coordinates(r, 0, names);    
     // Simulate
-    reb_integrate(r,100.);
+    for (int i = 0; i < iterations; i++)
+    {
+        printf("# Iteration %i\n", i);
+        simulate(r, timestep);
+        // Print time step (XYZ)
+        print_coordinates(r, i + 1, names);
+    }
+    printf("# End of simulation\n");
 
-    // Print results
-    for (int i=0; i<r->N; i++){
-        struct reb_particle p = r->particles[i];
-        printf("%f %f %f\n", p.x, p.y, p.z);
-    }
-    struct reb_particle primary = r->particles[0];
-    for (int i=1; i<r->N; i++){
-        struct reb_particle p = r->particles[i];
-        struct reb_orbit o = reb_tools_particle_to_orbit(r->G, p, primary);
-        printf("%f %f %f\n", o.a, o.e, o.f);
-    }
+    // Save
+    reb_output_binary(r, "snapshot.bin");
+    // Print coordinates around primary
+    printf("\n# Orbits: <Name> <Semi-major axis> <elevation> <true anomaly> \n");
+    print_orbits(r, names);
 
     // Release
     reb_free_simulation(r);
 }
-
